2c14e83d11eaa78ccf49fd782f88560263cc6bff
Vinicius Sousa <viniciussousa@wizsolucoes.com.br>
2023-03-13 20:21:23 -0300

    fix: subindo alterçaões

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -3,7 +3,6 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
 
 
 
--- src/app/features/dashboard/home/home.component.ts
+++ src/app/features/dashboard/home/home.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  value: any = '';
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+   buttonClick() {
+    const data = {test: 'e'};
+    if(data.test === 'e')  {
+
+      console.log('é e')
+    }else {
+      console.log('não é e')
+    }
+
+   }
 }
